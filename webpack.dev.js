96fae71358cd6f1e8b6d22a78fbea9930e247482
samandeveloper <sn.mohaghegh@gmail.com>
2020-07-04 12:14:31 -0600

    ok4

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
-const HtmlWebpackPlugin = require('./html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const common = require('./webpack.common');
-const merge = require(',/webpack-merge');
-const path = require('./path');
+const merge = require('webpack-merge');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
